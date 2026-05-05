0a9897b6cdd5d870d5cc994107b61ec03a00ed22
otoyo <hiroki.t.1988@gmail.com>
2026-05-05 17:13:15 +0900

    Format

--- src/lib/notion/client.ts
+++ src/lib/notion/client.ts
@@ -502,7 +502,10 @@ export async function getDatabase(): Promise<Database> {
         Type: dataSource.icon.type,
         Emoji: dataSource.icon.emoji,
       }
-    } else if (dataSource.icon.type === 'external' && 'external' in dataSource.icon) {
+    } else if (
+      dataSource.icon.type === 'external' &&
+      'external' in dataSource.icon
+    ) {
       icon = {
         Type: dataSource.icon.type,
         Url: dataSource.icon.external?.url || '',
@@ -536,7 +539,9 @@ export async function getDatabase(): Promise<Database> {
   return database
 }
 
-export async function _getDataSource(data_source_id: string): Promise<responses.DataSourceObject> {
+export async function _getDataSource(
+  data_source_id: string
+): Promise<responses.DataSourceObject> {
   const params: requestParams.RetrieveDataSource = {
     data_source_id: data_source_id,
   }
